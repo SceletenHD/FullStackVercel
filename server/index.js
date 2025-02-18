1dec4189c2bba26e038719f0704475468785b278
Luca Reinstadler <lucareinstadler@gmail.com>
2025-02-18 17:26:18 +0100

    cors

--- server/index.js
+++ server/index.js
@@ -1,8 +1,14 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+// Use CORS middleware
+app.use(cors());
 
 app.use("/", (req, res) => {
   res.send("Server is running.");
 });
 
-app.listen(5000, console.log("Server started on PORT 5000"));
+app.listen(5000, () => {
+  console.log("Server started on PORT 5000");
+});
